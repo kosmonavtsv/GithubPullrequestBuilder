43da8b608dce5b3c0de8b4cf750285295051e7ac
Semyon <kosmonavtsv@gmail.com>
2018-08-05 14:16:46 +0400

    Build pull request summary from commits

--- GithubPRBuilder/GithubPRBuilder.psm1
+++ GithubPRBuilder/GithubPRBuilder.psm1
@@ -1,4 +1,4 @@
-<#
+﻿<#
 .SYNOPSIS
     Create pull request
 .DESCRIPTION
@@ -25,7 +25,7 @@ function New-GithubPullrequest {
 
     $targetBranch = Get-GitBranchName -BranchRef $TargetRef
     $title = New-PRTitle -TargetBranch $targetBranch
-    $description = New-PRDescription
+    $description = New-PRDescription -Target $targetBranch -Base $Base
     $repository = Get-GitRepositoryName
 
     $pull = Send-GitubPullrequest `
@@ -96,6 +96,115 @@ function Get-GitRepositoryName {
 }
 
 function New-PRDescription {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Target,
+        [Parameter(Mandatory)]
+        [string]$Base
+    )
+    # return  Get-GithubPRTemplate 
+    $prTemplate = Get-ModulePRTemplate
+    $prTemplate = Fill-JiraParagraph -Template $prTemplate -Target $Target -Base $Base
+    $prTemplate = Fill-SolutionParagraph -Template $prTemplate -Target $Target -Base $Base
+    Write-Verbose "PR Description:`r`n$prTemplate"
+}
+
+function Fill-SolutionParagraph {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Target,
+        [Parameter(Mandatory)]
+        [string]$Base,
+        [Parameter(Mandatory)]
+        [string]$Template
+    )
+    $commits = Get-GitCommitsDiff -Target $Target -Base "origin/$Base"
+    #Write-Verbose $commits.Count()
+    Write-Verbose $commits[0].Title
+    $commits = $commits | % {
+        $body = "* $($_.Title.TrimEnd('.'))."
+        if ($commit.Body) {
+            $body += "`r`n`r`n$($_.Body)"
+        }
+        $body
+    }
+    $solutionParagraph = [string]::Join("`r`n", $commits)
+    $Template -replace '<solution>', $solutionParagraph
+}
+
+function Fill-JiraParagraph {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Target,
+        [Parameter(Mandatory)]
+        [string]$Base,
+        [Parameter(Mandatory)]
+        [string]$Template
+    )
+    function Add-JiraKeyFromBranch([string[]] $JiraKeys) {
+        if ($Target -match 'CASEM-\d+') {
+            $jiraKeys += $Matches[0]
+        }
+        $jiraKeys | select -Unique
+    }
+
+    function New-JiraParagraph([object[]]$JiraIssues) {
+        $jiraParagraph = ''
+        $jiraMarkdownLinks = $JiraIssues | % {"* [$($_.fields.summary)](https://jira.parcsis.org/browse/$($_.key))"}
+        if ($jiraMarkdownLinks) {
+            $jiraParagraph = [string]::Join("`r`n", $jiraMarkdownLinks)
+        }
+        return $jiraParagraph
+    }
+
+    if ($prTemplate.Contains('<jira>')) {
+        $jiraKeys = Get-JiraKeysFromCommits -Target $Target -Base $Base
+        $jiraKeys = Add-JiraKeyFromBranch -JiraKeys $jiraKeys
+        $jiraIssues = $jiraKeys | Get-JiraIssueSafe
+        $jiraParagraph = New-JiraParagraph -JiraIssues $jiraIssues
+        $Template -replace '<jira>', $jiraParagraph
+    }
+    else {
+        $Template
+    }
+}
+
+function Get-JiraIssueSafe {
+    param(
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string]$JiraKey
+    )
+    try {
+        Get-JiraIssue -IssueKey $JiraKey
+    }
+    catch {
+        Write-Warning "jira issue $JiraKey not found"
+    }
+}
+
+function Get-JiraKeysFromCommits {
+    [CmdletBinding()]
+    [OutputType([string[]])]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Target,
+        [Parameter(Mandatory)]
+        [string]$Base
+    )
+    $commits = Get-GitCommitsDiff -Target $Target -Base "origin/$Base"
+    $commits `
+        | ? {$_.Title -match 'CASEM-\d+' } `
+        | % {$Matches[0]} `
+        | select -Unique
+}
+
+function Get-GithubPRTemplate {
     $gitRoot = (git rev-parse --show-toplevel)
     $template = "$gitRoot\PULL_REQUEST_TEMPLATE.md"
     if (Test-Path $template) {
@@ -103,6 +212,83 @@ function New-PRDescription {
     }
     else {
         ""
+    }
+}
+
+class GitCommit {
+    [string] $Title
+    [string] $Description
+    GitCommit($Title, $Description) {
+        if ($Title) {
+            $this.Title = $Title.Trim()
+        }
+        if ($Description) {
+            $this.$Description = $Description.Trim()
+        }
+    }
+}
+
+function Get-GitCommitsDiff {
+    [CmdletBinding()]
+    [OutputType([GitCommit[]])]
+    param(
+        [Parameter(Mandatory = $false)]
+        [string]$Target = "HEAD",
+        [Parameter(Mandatory = $false)]
+        [string]$Base = "develop"
+    )
+    # We use delimiters, since the text can contain a carriage return code
+    # %s - commit's title
+    # %b - commit's description
+    $logFormat = "%s<commit_head_end>%b<commit_end>"
+    $log = Get-GitLog -Target $Target -Base $Base -LogFormat $logFormat
+    $commits = $log.Split(@("<commit_end>"), [System.StringSplitOptions]::RemoveEmptyEntries) 
+    #| % {$_.Trim("`r`n")}
+    foreach ($commit in $commits) {
+        $commitParts = $commit -split "<commit_head_end>"
+        [GitCommit]::new($commitParts[0], $commitParts[1])
+    }
+}
+
+function Get-GitLog {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        [Parameter(Mandatory)]
+        [string]$Target,
+        [Parameter(Mandatory)]
+        [string]$Base,
+        [Parameter(Mandatory)]
+        [string]$LogFormat
+    )
+    if ($Script:log) {
+        $log = $Script:log
+    }
+    else {
+        Write-Verbose "git fetch origin $Base"
+        $null = (git fetch origin $Base)
+        Write-Verbose "git log $Target --not $Base --quiet --pretty=$LogFormat  --reverse"
+        $log = (git log $Target --not $Base --quiet --pretty=$LogFormat  --reverse) -join "`r`n"
+        # Encoding
+        $utf8 = [System.Text.Encoding]::UTF8
+        $outputEncoding = [Console]::OutputEncoding
+        $log = $utf8.GetString($outputEncoding.GetBytes($log))
+        Write-Verbose "GIT COMMITS: $log"
+        $Script:log = $log
+    }
+    return $log
+}
+
+function Get-ModulePRTemplate {
+    [CmdletBinding()]
+    [OutputType([string])]
+
+    $config = Import-GithubPRBuilderConfiguration
+    if ($confog.$PRTemplatePath) {
+        Get-Content $confog.$PRTemplatePath -Raw
+    }
+    else {
+        $PRTemplate
     }
 }
 
@@ -127,6 +313,7 @@ function New-PRTitle {
             $title = "$targetBranch $($branchIssue.fields.summary)"
         }
     }
+    Write-Verbose "PR Title:`r`n$title"
     return $title
 }
 
@@ -188,7 +375,9 @@ function Update-GithubPRBuilderConfiguration {
         [switch]$JiraPassword,
         # Github
         [string]$GithubToken,
-        [string]$GithubOwner
+        [string]$GithubOwner,
+        # Other
+        [string]$PRTemplatePath
     )
     end {
         If ($PSCmdlet.ShouldProcess("Saving settings to $(Get-ConfigurationPath)")) {
@@ -222,7 +411,9 @@ function Set-GithubPRBuilderConfiguration {
         [Parameter(Mandatory)]
         [string]$GithubToken,
         [Parameter(Mandatory)]
-        [string]$GithubOwner
+        [string]$GithubOwner,
+        # Other
+        [string]$PRTemplatePath
     )
     end {
         If ($PSCmdlet.ShouldProcess("Saving settings to $(Get-ConfigurationPath)")) {
@@ -250,3 +441,25 @@ catch {
     # Hide the error on import, just warn them
     Write-Warning "You must configure module to avoid this warning on first run. Use Set-GithubPRBuilderConfiguration"
 }
+
+$PRTemplate = @"
+:ledger: JIRA
+--
+<jira>
+
+:fire: Задача\Проблема
+--
+Техническое описание проблемы или задачи.
+
+:bulb: Реализация
+--
+<solution>
+
+:checkered_flag: Тестирование
+--
+Какие тесты были написаны или по каким причинам не удалось написать тест.
+
+Зона аффекта
+---
+Не забыть указать зону аффекта баги в jira задаче. Это особенно необходимо если это баг в `prerelease` или задевает ту функциональность которая уже протестирована в фиче.
+"@
