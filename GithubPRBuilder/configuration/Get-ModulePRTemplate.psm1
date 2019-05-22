58561b3afb2bc478218e69c4c64d3463aa3a8afd
Semyon <kosmonavtsv@gmail.com>
2019-05-22 11:16:56 +0400

    Update Get-ModulePRTemplate.psm1

--- GithubPRBuilder/configuration/Get-ModulePRTemplate.psm1
+++ GithubPRBuilder/configuration/Get-ModulePRTemplate.psm1
@@ -3,8 +3,8 @@ function Get-ModulePRTemplate {
     [OutputType([string])]
 
     $config = Import-GithubPRBuilderConfiguration
-    if ($config.$PRTemplatePath) {
-        Get-Content $config.$PRTemplatePath -Raw
+    if ($config.PRTemplatePath) {
+        Get-Content $config.PRTemplatePath -Raw
     }
     else {
         $PRTemplate
